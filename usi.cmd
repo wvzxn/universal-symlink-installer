f1bb26355ae43719b11105e8d0518219dd9c1fc8
wvzxn <zvp.ukr01@gmail.com>
2022-12-01 18:00:00 +0200

    code improvements

--- usi.cmd
+++ usi.cmd
@@ -1,27 +1,37 @@
-:: Universal Symlink Installer v1.4f
-:: Author: wvzxn // https://github.com/wvzxn/universal-symlink-installer
+::         Name: Universal Symlink Installer v1.5
+::       Author: wvzxn // https://github.com/wvzxn/
+::  Description: Simple script that will help you speed up the Symlink creation process on Windows OS.
+::               It acts as both installer and uninstaller.
+::               Learn how to use at https://github.com/wvzxn/universal-symlink-installer#usage
+
 @echo off
-if not "%1"=="am_admin" ( powershell start -verb runas '%0' 'am_admin "%~1" "%~2"' & exit )
+net session >nul 2>&1
+if %errorLevel% NEQ 0 ( echo Run as Administrator required. & pause & exit )
 setlocal EnableDelayedExpansion
 cd /d "%~dp0"
-set "_line_=[=============================================================]"
-for %%A in (.) do set "x=%%~nxA"
-for /f "usebackq delims=" %%A in (` powershell "'%x%'.ToUpper()" `) do set "x=%%A"
-echo !_line_!
-powershell "$a='%x%'.length; $l=(57-$a)/2; $r=$l+((57-$a)%%2); Write-Host -nonewline ' --'; for($i=1; $i -le $l; $i++){Write-Host -nonewline ' '}; Write-Host -nonewline '%x%'; for($i=1; $i -le $r; $i++){Write-Host -nonewline ' '}" & echo --
-echo  --               Universal Symlink Installer               --
-echo !_line_!
+set "f0=%~f0"
+set "_=[=============================================================]"
+for %%A in (.) do ( for /f "usebackq delims=" %%I in (` powershell "'%%~nA'.ToUpper()" `) do set "n=%%I" )
 
-if exist .usi ( goto:symlinkInstalled )
-echo Press ^[Y^] to Install
-for /f "usebackq delims=" %%A in (` powershell "[Console]::ReadKey($true).Key" `) do if not "%%A"=="Y" ( exit )
-echo !_line_!
-for /f "usebackq delims=" %%A in (` findstr /b ::: "%~f0" `) do (
+echo !_!
+call:centerText Universal Symlink Installer
+call:centerText !n!
+echo !_!
+echo.
+if not exist .usi ( call:usiSet ) else ( call:usiDel )
+pause
+exit
+
+:usiSet
+call:centerText Press ^[Y^] to Install
+call:userPrompt Y
+echo.
+for /f "usebackq delims=" %%A in (` findstr /b /c:"::: " "!f0!" `) do (
     set "i=%%A"
     set i=!i:~4!
     set par=!i:~0,2!
-    for /f "usebackq delims=" %%I in (` powershell "if ('!par!' -like '/*' -or '!par!' -like 'C\') {echo 1} else {echo 2}" `) do (
-        if %%I EQU 2 ( for /f "usebackq delims=" %%J in (` powershell "'!i!'.replace('%%','%%')" `) do %%J ) else (
+    for /f "usebackq delims=" %%I in (` powershell "if('!par!' -match '(/.?)|(C\\)'){echo 1}else{echo 2}" `) do (
+        if %%I EQU 2 ( !i! ) else (
             if "!par!"=="//" ( echo !i:~3!>> .usi ) else (
                 if "!par!"=="C\" (
                     set "source=!i!"
@@ -42,34 +52,37 @@ for /f "usebackq delims=" %%A in (` findstr /b ::: "%~f0" `) do (
         )
     )
 )
-if exist .usi (
-    attrib +h .usi
-    icacls .usi /deny *S-1-1-0:F /Q 1>nul
-)
-echo !_line_!
-goto:end
+if exist .usi ( attrib +r +h "%~dp0.usi" >nul 2>&1 & icacls "%~dp0.usi" /deny *S-1-1-0:F >nul 2>&1 )
+exit /b
 
-:symlinkInstalled
-echo Press ^[Enter^] to Uninstall
-for /f "usebackq delims=" %%A in (` powershell "[Console]::ReadKey($true).Key" `) do if not "%%A"=="Enter" ( exit )
-echo !_line_!
-icacls .usi /reset /Q 1>nul
-attrib -h .usi
-echo Removing...
+:usiDel
+call:centerText Press ^[Enter^] to Uninstall
+call:userPrompt Enter
+echo.
+icacls "%~dp0.usi" /reset >nul 2>&1 & attrib -r -h "%~dp0.usi" >nul 2>&1
 for /f "tokens=*" %%A in (.usi) do (
-    set "par=%%A" & set par=!par:~0,2!
-    if "!par!"=="C:" (
+    set "par=%%A"
+    if "!par:~0,2!"=="C:" (
         echo - %%A
         if exist "%%A\*" ( rd /q "%%A" ) else ( del /q "%%A" )
     ) else ( %%A )
 )
 del /q /f .usi
-echo !_line_!
+exit /b
 
-:end
-setlocal DisableDelayedExpansion
-echo Done!
-pause
+:centerText
+powershell "$a='%*'.length;$l=(63-$a)/2;for($i=1;$i -le $l;$i++){Write-Host -nonewline ' '}"
+echo %*
+exit /b
 
-::: echo Please read the manual
-::: start "" https://github.com/wvzxn/universal-symlink-installer#usage
+:userPrompt
+for /f "usebackq delims=" %%K in (` powershell "[Console]::ReadKey($true).Key" `) do if not "%%K"=="%*" ( exit )
+exit /b
+
+::: cls
+::: echo !_!
+::: powershell "gc '!f0!'|%{if($_ -match '(^^::\s\s)(.*)'){echo $_}}"
+::: echo !_!
+::: pause
+::: start https://github.com/wvzxn/universal-symlink-installer#usage
+::: exit
