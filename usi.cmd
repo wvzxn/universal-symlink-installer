62f76cd360a691ac1518117e11af6fb39c2a7bb2
wvzxn <zvp.ukr01@gmail.com>
2022-12-11 22:00:53 +0200

    added regex installer feature; code refactorings

--- usi.cmd
+++ usi.cmd
@@ -1,4 +1,4 @@
-::         Name: Universal Symlink Installer v1.5j
+::         Name: Universal Symlink Installer v1.6
 ::       Author: wvzxn // https://github.com/wvzxn/
 ::  Description: Simple script that will help you speed up the Symlink creation process on Windows.
 ::               It acts as both installer and uninstaller.
@@ -8,8 +8,9 @@
 net session >nul 2>&1
 if %errorLevel% NEQ 0 ( echo Run as Administrator required. & pause & exit )
 setlocal EnableDelayedExpansion
-cd /d "%~dp0"
+set "dp0=%~dp0"
 set "f0=%~f0"
+cd /d "!dp0!"
 set "_=[=============================================================]"
 for %%A in (.) do ( for /f "usebackq delims=" %%I in (` powershell "'%%~nA'.ToUpper()" `) do set "n=%%I" )
 
@@ -18,7 +19,7 @@ call:centerText Universal Symlink Installer
 call:centerText !n!
 echo !_!
 echo.
-if not exist .usi ( call:usiSet ) else ( call:usiDel )
+if not exist .usi ( call:usiSet) else ( call:usiDel)
 pause
 exit
 
@@ -28,32 +29,51 @@ call:userPrompt Y
 echo.
 for /f "usebackq delims=" %%A in (` findstr /b /c:"::: " "!f0!" `) do (
     set "i=%%A"
-    set i=!i:~4!
-    set par=!i:~0,2!
-    for /f "usebackq delims=" %%I in (` powershell "if('!par!' -match '(/.?)|(C\\)'){echo 1}else{echo 2}" `) do (
-        if %%I EQU 2 ( !i! ) else (
-            if "!par!"=="//" ( echo !i:~3!>> .usi ) else (
-                if "!par!"=="C\" (
-                    set "source=!i!"
-                    if exist "%~dp0!source!\*" ( set "par=/d " ) else ( set "par=" )
-                ) else (
-                    set source=!i:~3!
-                    set "par=!par! "
-                )
-                set "link=C:\!source:~2!"
-                set link=!link:^(Name^)=%username%!
-                set "source=%~dp0!source!"
-                for /f "usebackq delims=" %%J in (` powershell "Split-Path '!link!' -Parent" `) do if not exist "%%J" ( md "%%J" )
-                if exist "!link!" ( echo !link! exists ) else (
-                    mklink !par!"!link!" "!source!"
-                    echo !link!>> .usi
-                )
+    set "i=!i:~4!"
+    set "par=!i:~0,2!"
+    for /f "usebackq delims=" %%I in (` powershell "'!par!' -match '(\/.?)|(C\\)'" `) do (
+        if "%%I"=="False" ( !i! ) else (
+            if "!par!"=="//" ( echo !i:~3!>> .usi) else (
+                if "!par!"=="/r" ( call:usiSet_parDestSrc_regex) else ( call:usiSet_parDestSrc)
             )
         )
     )
 )
 if exist .usi ( attrib +r +h "%~dp0.usi" >nul 2>&1 & icacls "%~dp0.usi" /deny *S-1-1-0:F >nul 2>&1 )
 exit /b
+
+:usiSet_parDestSrc
+if "!par!"=="C\" (
+    set "src=!i!"
+    if exist "%~dp0!src!\*" ( set "par=/d ") else ( set "par=")
+) else (
+    set "src=!i:~3!"
+    set "par=!par! "
+)
+call:usiSet_mklink
+exit /b
+
+:usiSet_parDestSrc_regex
+if "!i:~0,4!"=="/r /" (
+    set "par=!i:~3,3!"
+    set "i=!i:~6!"
+) else (
+    set "par=/d "
+    set "i=!i:~3!"
+)
+for /f "usebackq delims=" %%J in (` powershell "$a=gci '!dp0!C' -directory -recurse|?{$_.name -match '!i!' -and $_.fullname -notmatch 'C\\.*?!i!.*!i!'};$a.fullname|%%{$_ -replace '!dp0:\=\\!',''}" `) do (
+    set "src=%%J"
+    call:usiSet_mklink
+)
+exit /b
+
+:usiSet_mklink
+set "dest=C:!src:~1!"
+set "dest=!dest:(Name)=%username%!"
+for /f "usebackq delims=" %%K in (` powershell "split-path '!dest!' -parent" `) do if not exist "%%K" ( md "%%K")
+mklink !par!"!dest!" "!dp0!!src!"
+if %errorlevel% EQU 0 ( echo !dest!>> .usi)
+exit /b 0
 
 :usiDel
 call:centerText Press ^[Enter^] to Uninstall
@@ -64,7 +84,7 @@ for /f "tokens=*" %%A in (.usi) do (
     set "par=%%A"
     if "!par:~0,2!"=="C:" (
         echo - %%A
-        if exist "%%A\*" ( rd /q "%%A" ) else ( del /q "%%A" )
+        if exist "%%A\*" ( rd /q "%%A") else ( del /q "%%A")
     ) else ( %%A )
 )
 del /q /f .usi
@@ -76,7 +96,7 @@ echo %*
 exit /b
 
 :userPrompt
-for /f "usebackq delims=" %%K in (` powershell "[Console]::ReadKey($true).Key" `) do if not "%%K"=="%*" ( exit )
+for /f "usebackq delims=" %%J in (` powershell "[Console]::ReadKey($true).Key" `) do if not "%%J"=="%*" ( exit )
 exit /b
 
 :::: [-------------------------------------------------------------] ::::
@@ -89,4 +109,3 @@ exit /b
 ::: echo !_!
 ::: pause
 ::: start https://github.com/wvzxn/universal-symlink-installer#usage
-::: exit
